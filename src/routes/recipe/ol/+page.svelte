ac9036eb6e20e318f779ea4b158461460c6b3f44
Lukas-Siegle <sieglelukas1@gmail.com>
2024-10-30 09:58:49 +0100

    remove old code

--- src/routes/recipe/ol/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import type { PageData } from "../$types.js";
-    import NewRecipe from "./NewRecipe.svelte";
-    export let data: PageData;
-</script>
-
-<NewRecipe data={data.form} />
